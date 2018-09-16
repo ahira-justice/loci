9a5a9294b36afb3dc4d4285bdc141383cb9f3da3
Adddefokun Ahira Justice <justiceahira@gmail.com>
2018-09-16 22:55:16 +0100

    Current work on first version (MVP). User vs AI.

---
+++ inout.py
@@ -0,0 +1,35 @@
+"""
+    Ahira Justice, ADEFOKUN
+    justiceahira@gmail.com
+"""
+
+
+import os
+import sys
+
+
+def readFile(filename):
+    assert os.path.exists(filename), 'Cannot find the file: %s' % (filename)
+    file = open(filename, 'r')
+    content = []
+    for line in file:
+        newLine = line.split()
+        content.append(newLine)
+    file.close()
+    return content
+
+
+def writeToFile(output, filename):
+    file = open(filename, 'w')
+    file.write(output)
+    file.close()
+    sys.exit()
+
+
+def writeToString(output, out_print):
+    for item in output:
+        item = str(item)
+        out_print += item + '\t'
+    out_print += '\n'
+
+    return out_print
--- loci.py
+++ loci.py
@@ -1,8 +1,175 @@
 """
-    Adefokun Ahira Justice
+    Ahira Justice, ADEFOKUN
     justiceahira@gmail.com
 """
+
 
 import os
 import sys
 import chess
+
+
+AI = False
+PLAYER = True
+
+
+def reverseList(_list):
+    return _list[::-1]
+
+
+pawnEvalWhite = [
+    [0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
+    [5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0],
+    [1.0,  1.0,  2.0,  3.0,  3.0,  2.0,  1.0,  1.0],
+    [0.5,  0.5,  1.0,  2.5,  2.5,  1.0,  0.5,  0.5],
+    [0.0,  0.0,  0.0,  2.0,  2.0,  0.0,  0.0,  0.0],
+    [0.5, -0.5, -1.0,  0.0,  0.0, -1.0, -0.5,  0.5],
+    [0.5,  1.0, 1.0,  -2.0, -2.0,  1.0,  1.0,  0.5],
+    [0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0]
+]
+
+pawnEvalBlack = reverseList(pawnEvalWhite)
+
+bishopEvalWhite = [
+    [ -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0],
+    [ -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0],
+    [ -1.0,  0.0,  0.5,  1.0,  1.0,  0.5,  0.0, -1.0],
+    [ -1.0,  0.5,  0.5,  1.0,  1.0,  0.5,  0.5, -1.0],
+    [ -1.0,  0.0,  1.0,  1.0,  1.0,  1.0,  0.0, -1.0],
+    [ -1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0, -1.0],
+    [ -1.0,  0.5,  0.0,  0.0,  0.0,  0.0,  0.5, -1.0],
+    [ -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0]
+]
+
+bishopEvalBlack = reverseList(bishopEvalWhite)
+
+rookEvalWhite = [
+    [  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
+    [  0.5,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [  0.0,   0.0, 0.0,  0.5,  0.5,  0.0,  0.0,  0.0]
+]
+
+rookEvalBlack = reverseList(rookEvalWhite)
+
+knightEval = [
+    [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0],
+    [-4.0, -2.0,  0.0,  0.0,  0.0,  0.0, -2.0, -4.0],
+    [-3.0,  0.0,  1.0,  1.5,  1.5,  1.0,  0.0, -3.0],
+    [-3.0,  0.5,  1.5,  2.0,  2.0,  1.5,  0.5, -3.0],
+    [-3.0,  0.0,  1.5,  2.0,  2.0,  1.5,  0.0, -3.0],
+    [-3.0,  0.5,  1.0,  1.5,  1.5,  1.0,  0.5, -3.0],
+    [-4.0, -2.0,  0.0,  0.5,  0.5,  0.0, -2.0, -4.0],
+    [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0]
+]
+
+evalQueen = [
+    [ -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0],
+    [ -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0],
+    [ -1.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0],
+    [ -0.5,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5],
+    [  0.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5],
+    [ -1.0,  0.5,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0],
+    [ -1.0,  0.0,  0.5,  0.0,  0.0,  0.0,  0.0, -1.0],
+    [ -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0]
+]
+
+kingEvalWhite = [
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -2.0, -3.0, -3.0, -4.0, -4.0, -3.0, -3.0, -2.0],
+    [ -1.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -1.0],
+    [  2.0,  2.0,  0.0,  0.0,  0.0,  0.0,  2.0,  2.0 ],
+    [  2.0,  3.0,  1.0,  0.0,  0.0,  1.0,  3.0,  2.0 ]
+]
+
+kingEvalBlack = reverseList(kingEvalWhite)
+
+def printBoard(board):
+    print()
+    print(board)
+
+def getAbsoluteValue(piece, piece_color, x, y):
+    if (piece.piece_type == chess.PAWN):
+        return 10 + (pawnEvalWhite[y][x] if piece_color is chess.WHITE else pawnEvalBlack[y][x])
+    elif (piece.piece_type == chess.ROOK):
+        return 50 + (rookEvalWhite[y][x] if piece_color is chess.WHITE else rookEvalBlack[y][x])
+    elif (piece.piece_type == chess.KNIGHT):
+        return 50 + knightEval[y][x]
+    elif (piece.piece_type == chess.BISHOP):
+        return 50 + (bishopEvalWhite[y][x] if piece_color is chess.WHITE else bishopEvalBlack[y][x])
+    elif (piece.piece_type == chess.QUEEN):
+        return 50 + evalQueen[y][x]
+    elif (piece.piece_type == chess.KING):
+        return 50 + (kingEvalWhite[y][x] if piece_color is chess.WHITE else kingEvalBlack[y][x])
+    else:
+        assert "Unknown piece type: " + piece.piece_type
+        sys.exit()
+
+
+def getPieceValue(piece, x, y):
+    if (piece is None):
+        return 0
+
+    absoluteValue = getAbsoluteValue(piece, piece.color, x, y)
+    return absoluteValue if piece.color is chess.WHITE else -absoluteValue
+
+
+def evaluateBoard(board):
+    totalEvaluation = 0
+
+    for i in list(range(8)):
+        for j in list(range(8)):
+            totalEvaluation += getPieceValue(board.piece_at(chess.square(j, i)), i, j)
+
+    return totalEvaluation
+
+
+def makeBestMove():
+    return
+
+
+def getBestMove():
+    return
+
+
+def getUserInput():
+    return input("\nPlayer turn: ")
+
+
+def main():
+    board = chess.Board()
+
+    print("\n-----START-----")
+    printBoard(board)
+
+    while not board.is_checkmate():
+
+        if board.turn is PLAYER:
+            user_input = getUserInput()
+            move = chess.Move.from_uci(user_input)
+
+            if user_input is 'quit':
+                break
+
+            if move in board.legal_moves:
+                board.push(move)
+            else:
+                print("Invalid Move.")
+
+        elif board.turn is AI:
+            value = evaluateBoard(board)
+            print("\n" + str(value))
+            board.turn = PLAYER
+        
+        printBoard(board)
+
+
+if __name__ == '__main__':
+    main()
